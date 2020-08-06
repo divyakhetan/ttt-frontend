7a4519acc71a5eaa3e3986afd25c0d5ae9c3444f
divyakhetan <divyakhetan1209@gmail.com>
2020-08-06 16:53:50 +0530

    Updates

---
+++ precache-manifest.2c294a2ad12cc9086ca3209d603cb7d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb18baabb3a531f59e458e8ed5bf3eb",
+    "url": "/ttt-frontend/index.html"
+  },
+  {
+    "revision": "7765e4648f8f286b22c6",
+    "url": "/ttt-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98141c06651500b57105",
+    "url": "/ttt-frontend/static/js/2.da5a3b67.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ttt-frontend/static/js/2.da5a3b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7765e4648f8f286b22c6",
+    "url": "/ttt-frontend/static/js/main.5474e27e.chunk.js"
+  },
+  {
+    "revision": "c514d8cca46dbe4b34cb",
+    "url": "/ttt-frontend/static/js/runtime-main.31b03a04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ttt-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
